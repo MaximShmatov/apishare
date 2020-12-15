0b1bc7880a5029a2df376cdca3bd23057cef583e
MaximShmatov <ExternGroupMail@mail.ru>
2020-12-15 11:45:50 +0300

    test 2

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
+  res.send('TEST 2');
 });
 app.listen({ port }, () =>
   console.log(`Server running at port:${port}`)
