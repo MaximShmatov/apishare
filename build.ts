bf95d8c6f2144738495877683dfc33cf02b25be9
MaximShmatov <ExternGroupMail@mail.ru>
2020-12-17 02:15:12 +0300

    Editing app.ts

--- build.ts
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-
-fs.mkdirSync('build/addons/earcut/earcut.node', { recursive: true });
-fs.createReadStream('src/addons/earcut/earcut.node')
-  .pipe(fs.createWriteStream('build/addons/earcut/earcut.node'));
--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,12 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/triangulation_box', (req, res) => {
-  // res.json(
-  //   new TriangulationBox(
-  //     Number(req.query.width),
-  //     Number(req.query.height),
-  //     Number(req.query.dept))
-  // );
-  res.send(req.query.width);
+  res.json(
+    new TriangulationBox(
+      Number(req.query.width),
+      Number(req.query.height),
+      Number(req.query.dept))
+  );
 });
 
 app.listen({port}, () =>
